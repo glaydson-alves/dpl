4a94f2adbd13b9bd88cc184c7e1e44aed324d4a4
glaydson-alves <glaydson.alves36@gmail.com>
2023-10-13 23:05:00 -0300

    iniciado historico

--- src/js/form.js
+++ src/js/form.js
@@ -52,19 +52,5 @@ function voltarParaInicio() {
     }, 500);
 }
 
-// function iniciar() {
-//     // Obtém as seções
-//     const inicioSection = document.querySelector('.inicio');
-//     const registroSection = document.querySelector('.registro');
 
-//     // Adiciona a classe "hidden" para ocultar a seção de início
-//     inicioSection.classList.add('hidden');
 
-//     // Exibe a seção de registro
-//     registroSection.style.display = 'block';
-
-//     // Aguarda um pequeno intervalo antes de remover a seção de início
-//     setTimeout(function() {
-//         inicioSection.style.display = 'none';
-//     }, 500); // 500ms para corresponder à transição de 0.5s
-// }
